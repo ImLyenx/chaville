ab71200d4bceb60a050e71617848fa5ec5fcbfa4
Mathilde Morell <morell.mathilde74@gmail.com>
2025-02-13 12:09:36 +0100

    create page.tsx

---
+++ app/cesu/page.tsx
@@ -0,0 +1,30 @@
+export default function cesu() {
+    return (
+        <div>Le CESU
+        Qu’est-ce que le CESU ?
+        
+        Le Cesu est une offre simplifiée pour déclarer facilement la rémunération de votre salarié à domicile pour des activités de service à la personne. Le Cesu concerne tous les particuliers à différents moments de leur vie, pour améliorer le quotidien, pour la prise en charge de quelques heures de ménage ou pour accompagner une personne âgée ou handicapée.
+        
+        Le CESU c’est pour qui ?
+        
+        Le Cesu vous facilite les démarches administratives pour déclarer la rémunération versée à un salarié à domicile pour des activités de services à la personne telles que:
+        l’entretien de la maison,
+        les petits travaux de jardinage et bricolage,
+        le soutien scolaire (à l’exception des cours à distance qui ne sont pas éligibles au Cesu),
+        le babysitting,
+        l’assistance aux personnes âgées ou fragiles à l’exception de soins relevant d’actes médicaux,
+        toutes les activités qui s’exercent en dehors du domicile dans le prolongement d’une activité de service au domicile de l’employeur.
+        Attention, vous ne pouvez pas notamment utiliser le Cesu pour :
+        les travaux de rénovation de l’habitat,
+        l’emploi d’une assistante maternelle agréée.
+        
+        
+        Est-ce obligatoire ?
+        
+        Dès que vous faites appel à un salarié à votre domicile, vous avez l’obligation de le déclarer. Votre déclaration permet au Cesu de calculer les cotisations, d’établir et d’adresser le bulletin de salaire à votre salarié. Ces cotisations sont les principales bases du financement solidaire de la Sécurité sociale.
+        En tant que salarié, être déclaré vous garantit des droits et vous permet de bénéficier d’une couverture sociale : assurance maladie, maternité, retraite, accident du travail, chômage… Ne pas être déclaré, c’est vous pénaliser dans l’obtention de vos droits sociaux.
+        Ne pas déclarer, établir une fausse déclaration ou ne pas déclarer toutes les heures effectuées, c’est encourir une sanction pénale et civile.
+        Pour plus d’informations, veuillez regarder le site web : https://www.cesu.urssaf.fr/info/accueil/s-informer-sur-le-cesu/tout-savoir/c-est-quoi-pour-qui.html
+        </div>
+    );
+}
