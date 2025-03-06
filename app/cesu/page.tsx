d6602ec60d78c6c6a78581246a095c2247c36268
Mathilde Morell <morell.mathilde74@gmail.com>
2025-03-06 10:14:10 +0100

    feat : page cesu + info

--- app/cesu/page.tsx
+++ app/cesu/page.tsx
@@ -1,30 +1,42 @@
 export default function cesu() {
     return (
-        <div>Le CESU
-        Qu’est-ce que le CESU ?
-        
-        Le Cesu est une offre simplifiée pour déclarer facilement la rémunération de votre salarié à domicile pour des activités de service à la personne. Le Cesu concerne tous les particuliers à différents moments de leur vie, pour améliorer le quotidien, pour la prise en charge de quelques heures de ménage ou pour accompagner une personne âgée ou handicapée.
-        
-        Le CESU c’est pour qui ?
-        
-        Le Cesu vous facilite les démarches administratives pour déclarer la rémunération versée à un salarié à domicile pour des activités de services à la personne telles que:
-        l’entretien de la maison,
-        les petits travaux de jardinage et bricolage,
-        le soutien scolaire (à l’exception des cours à distance qui ne sont pas éligibles au Cesu),
-        le babysitting,
-        l’assistance aux personnes âgées ou fragiles à l’exception de soins relevant d’actes médicaux,
-        toutes les activités qui s’exercent en dehors du domicile dans le prolongement d’une activité de service au domicile de l’employeur.
-        Attention, vous ne pouvez pas notamment utiliser le Cesu pour :
-        les travaux de rénovation de l’habitat,
-        l’emploi d’une assistante maternelle agréée.
-        
-        
-        Est-ce obligatoire ?
-        
-        Dès que vous faites appel à un salarié à votre domicile, vous avez l’obligation de le déclarer. Votre déclaration permet au Cesu de calculer les cotisations, d’établir et d’adresser le bulletin de salaire à votre salarié. Ces cotisations sont les principales bases du financement solidaire de la Sécurité sociale.
-        En tant que salarié, être déclaré vous garantit des droits et vous permet de bénéficier d’une couverture sociale : assurance maladie, maternité, retraite, accident du travail, chômage… Ne pas être déclaré, c’est vous pénaliser dans l’obtention de vos droits sociaux.
-        Ne pas déclarer, établir une fausse déclaration ou ne pas déclarer toutes les heures effectuées, c’est encourir une sanction pénale et civile.
-        Pour plus d’informations, veuillez regarder le site web : https://www.cesu.urssaf.fr/info/accueil/s-informer-sur-le-cesu/tout-savoir/c-est-quoi-pour-qui.html
-        </div>
+        <section className="m-10">
+            <div className="flex justify-center"><h1 className="text-4xl font-bold">CESU</h1></div>
+            <div>
+                <h2 className="font-bold">Qu’est-ce que le CESU ?</h2>
+                <p className="m-5 w-2/5">
+                    Le Cesu est une offre simplifiée pour déclarer facilement la rémunération de votre salarié à domicile pour des activités de service à la personne. Le Cesu concerne tous les particuliers à différents moments de leur vie, pour améliorer le quotidien, pour la prise en charge de quelques heures de ménage ou pour accompagner une personne âgée ou handicapée.
+                </p>
+
+                <h2 className="font-bold">Le CESU c’est pour qui ?</h2>
+                <p className="m-5 w-2/5">
+                Le Cesu vous facilite les démarches administratives pour déclarer la rémunération versée à un salarié à domicile pour des activités de services à la personne telles que:
+                </p>
+                <ol className="list-disc m-5">
+                    <li className="ml-10">l’entretien de la maison,</li>
+                    <li className="ml-10">les petits travaux de jardinage et bricolage,</li>
+                    <li className="ml-10">le soutien scolaire (à l’exception des cours à distance qui ne sont pas éligibles au Cesu),</li>
+                    <li className="ml-10">le babysitting,</li>
+                    <li className="ml-10">l’assistance aux personnes âgées ou fragiles à l’exception de soins relevant d’actes médicaux,</li>
+                    <li className="ml-10">toutes les activités qui s’exercent en dehors du domicile dans le prolongement d’une activité de service au domicile de l’employeur.</li>
+                </ol>
+
+                <h2 className="font-bold">Attention, vous ne pouvez pas notamment utiliser le Cesu pour :</h2>
+                <ol className="list-disc m-5">
+                    <li className="ml-10">les travaux de rénovation de l’habitat,</li>
+                    <li className="ml-10">l’emploi d’une assistante maternelle agréée.</li>
+                </ol>
+
+                <h2 className="font-bold">Est-ce obligatoire ?</h2>
+                <p className="m-5 w-2/5">
+                Dès que vous faites appel à un salarié à votre domicile, vous avez l’obligation de le déclarer. Votre déclaration permet au Cesu de calculer les cotisations, d’établir et d’adresser le bulletin de salaire à votre salarié. Ces cotisations sont les principales bases du financement solidaire de la <a className="italic hover:bg-slate-300 p-2 transition duration-150 rounded-lg" href="https://www.cesu.urssaf.fr/info/accueil/lexique.html?letter=S#securite-sociale">Sécurité sociale.</a>
+                </p>
+                <p>En tant que salarié, être déclaré vous garantit des droits et vous permet de bénéficier d’une couverture sociale : assurance maladie, maternité, <a className="italic hover:bg-slate-300 p-2 transition duration-150 rounded-lg" href="https://www.cesu.urssaf.fr/info/accueil/lexique.html?letter=R#retraite">retraite</a>, accident du travail, chômage… Ne pas être déclaré, c’est vous pénaliser dans l’obtention de vos droits sociaux.
+                Ne pas déclarer, établir une fausse déclaration ou ne pas déclarer toutes les heures effectuées, c’est encourir une sanction pénale et civile.
+                </p>
+                <br></br>
+                <h2 className="font-bold">Pour plus d’informations, veuillez regarder le site web : <a className="italic hover:bg-slate-300 p-2 transition duration-150 rounded-lg" href="https://www.cesu.urssaf.fr/info/accueil/s-informer-sur-le-cesu/tout-savoir/c-est-quoi-pour-qui.html">S'informer sur le CESU</a></h2>
+            </div>
+        </section>
     );
 }
