fc1fa1dd0b95f5a39f293a850ea7bcc121084568
Lyenx <105422453+ImLyenx@users.noreply.github.com>
2025-03-18 09:57:37 +0100

    feat: file upload

--- app/admin/blog/new/page.tsx
+++ app/admin/blog/new/page.tsx
@@ -1,7 +1,10 @@
+import ClientUploadButton from "@/components/upload-button";
+
 export default function NewPost() {
   return (
     <div>
       <h1>Nouveau post</h1>
+      <ClientUploadButton />
     </div>
   );
 }
